0984347fdf0b20a897589c3c5f537b1fdc2d00b1
Nauman Khan <naumankhan642@gmail.com>
2022-07-06 23:39:39 +0500

    Covered [4.0 & 4.5] parameters and strong params

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,4 +7,23 @@ class ClientsController < ApplicationController
     @client = Client.new
   end
 
+  def create
+    @client = Client.new(student_params)
+    if @client.save
+      redirect_to @client
+    else
+      render :new
+    end
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  private
+
+  def student_params
+    params.require(:client).permit(:name, :email, :status)
+  end
+
 end
